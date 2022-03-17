f77450dc84979db792fcd7be037003217d2fbbf7
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-03-17 23:12:08 +0630

    feat: Add handler function logic and check condition

--- components/Explore/PriceRange.js
+++ components/Explore/PriceRange.js
@@ -3,6 +3,30 @@ import { AiOutlineDollarCircle } from 'react-icons/ai'
 
 const PriceRange = () => {
     const [showPriceRangeForm, setShowPriceRangeForm] = useState(false)
+    const [fromPrice, setFromPrice] = useState('')
+    const [toPrice, setToPrice] = useState('')
+    const [error, setError] = useState(null)
+
+    const handleClickApplyButton = () => {
+        console.log(fromPrice, toPrice)
+
+        if (toPrice === '' && fromPrice === '') {
+            setError('Please fill one of the fields.')
+            return
+        }
+
+        if (+toPrice && +toPrice < +fromPrice) {
+            setError('Second price must be greater.')
+            return
+        }
+
+        setError('')
+    }
+
+    const handleClickClearButton = () => {
+        setFromPrice('')
+        setToPrice('')
+    }
 
     return (
         <div className="relative">
@@ -16,24 +40,35 @@ const PriceRange = () => {
 
             {showPriceRangeForm && (
                 <div className="absolute py-3 px-5 top-full mt-5  ml-10 z-20 bg-white text-black rounded-lg  ">
+                    {error && <p className="text-red-400">{error}</p>}
                     <p className="text-center py-2 font-bold">USD</p>
                     <div className="flex flex-col gap-3">
                         <input
                             className="border-2 border-gray-400 px-4 py-2 rounded-lg"
                             type="text"
                             placeholder="From"
+                            onChange={(e) => setFromPrice(e.target.value)}
+                            value={fromPrice}
                         />
                         <input
                             className="border-2 border-gray-400 px-4 py-2 rounded-lg"
                             type="text"
                             placeholder="To"
+                            onChange={(e) => setToPrice(e.target.value)}
+                            value={toPrice}
                         />
                     </div>
                     <div className="flex justify-between mt-5">
-                        <button className="px-5 py-2 bg-gray-400 text-black rounded-lg">
+                        <button
+                            onClick={handleClickClearButton}
+                            className="px-5 py-2 bg-gray-400 text-black rounded-lg"
+                        >
                             Clear
                         </button>
-                        <button className="px-5 py-2 bg-button-blue text-white rounded-lg">
+                        <button
+                            onClick={handleClickApplyButton}
+                            className="px-5 py-2 bg-button-blue text-white rounded-lg"
+                        >
                             Apply
                         </button>
                     </div>
