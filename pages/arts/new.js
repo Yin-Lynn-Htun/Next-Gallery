43dc9e1bb1e1cce70aae1bf7be693cb45155d6d2
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-03-19 23:39:49 +0630

    feat: Remove bg for layout

--- pages/arts/new.js
+++ pages/arts/new.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import Wrapper from '../../components/Wrapper'
 import PrimaryButton from '../../components/PrimaryButton'
+import SecondaryButton from '../../components/SecondaryButton'
 
 const FormInput = ({ name = 'Art Name' }) => (
-    <div className="flex flex-col">
-        <label htmlFor="item-name" className="my-3">
+    <div className="flex flex-col text-white">
+        <label htmlFor="item-name" className="my-3 text-lg">
             {name}
         </label>
         <input
             type="text"
             id="item-name"
             placeholder="Example: Monalisa"
-            className="p-3 border-black border-2"
+            className="p-3 border-black border-2 rounded-md"
         />
     </div>
 )
@@ -23,16 +24,20 @@ const CreatArt = () => {
                 <div className=" rays gradient_text text-4xl font-bold text-center mx-auto mb-4">
                     Upload your item
                 </div>
-                <div className="flex h-[1000px] bg-red-300">
-                    <div className="h-full bg-white flex-1 p-10">
+                <div className="flex h-[1000px] ">
+                    <div className="h-full  flex-1 p-10">
                         <div className="flex flex-col">
-                            <div className="border-dashed border-2 border-black h-40 w-full bg-red-300 grid place-items-center">
-                                Image placeholder
+                            <div className="text-white border-dashed border-2 border-white h-60 w-full bg-black grid place-items-center">
+                                <h1 className="text-2xl">
+                                    Drop your art image here
+                                </h1>
+                                <h3 className="text-large font-bold">OR</h3>
+                                <SecondaryButton>Browse File</SecondaryButton>
                             </div>
 
-                            <div className="bg-blue-200 flex flex-col w-full  ">
-                                <div className="bg-red-100 w-full my-4">
-                                    <h1 className="bg-black text-white py-3  text-xl px-10 w-full">
+                            <div className=" flex flex-col w-full my-8">
+                                <div className="w-full my-5">
+                                    <h1 className="bg-gray text-white py-3 text-xl px-10 w-full">
                                         item
                                     </h1>
                                     <div className="grid grid-cols-2 grid-rows-2 gap-5 px-10">
@@ -44,18 +49,18 @@ const CreatArt = () => {
                                     </div>
                                 </div>
 
-                                <div className="bg-red-100 w-full my-4">
-                                    <h1 className="bg-black text-white py-3  text-xl px-10 w-full">
+                                <div className="w-full my-4">
+                                    <h1 className="bg-gray text-white py-3  text-xl px-10 w-full">
                                         Property
                                     </h1>
                                     <div className="grid grid-cols-2 grid-rows-2 gap-5 px-10">
                                         <FormInput name="Price" />
                                         <FormInput name="Currency" />
-                                        <div className="col-span-2">
-                                            <h1 className="my-3">
+                                        <div className="col-span-2 text-white">
+                                            <h1 className="my-3 text-lg">
                                                 Choose categories
                                             </h1>
-                                            <button className="border-2 self border-black px-4 py-1">
+                                            <button className="border-2 self border-white px-4 py-1">
                                                 +
                                             </button>
                                         </div>
@@ -67,7 +72,7 @@ const CreatArt = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="w-[300px] bg-yellow-300 h-full">
+                    <div className="w-[300px] bg-yellow-300 h-full pt-10">
                         <h1>Sample Image</h1>
                     </div>
                 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
                 'border-blue': '#10599f',
                 'background-main': '#030812',
                 'text-pink': '#FD3E71',
+                gray: '#252525',
             },
 
             fontFamily: {
