09fc89379349699e563815ceec42e326e04adf78
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-03-21 00:13:51 +0630

    feat: Create Tag component

---
+++ components/Tag.js
@@ -0,0 +1,8 @@
+const Tag = ({ name }) => {
+    return (
+        <span className="bg-emerald-700 text-white w-max py-1 px-4 rounded-full m-2 whitespace-nowrap">
+            {name}
+        </span>
+    )
+}
+export default Tag
