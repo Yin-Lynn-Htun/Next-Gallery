bb8095dbe988e4f48287ce22ec552f8a706beea3
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-03-21 23:33:12 +0630

    feat: Remove image hover effect

--- components/HomePage/TodayPick.js
+++ components/HomePage/TodayPick.js
@@ -4,36 +4,36 @@ import Image from 'next/image'
 
 const TodayPick = () => {
     return (
-        <div className="text-white w-full">
-            <h1 className="text-3xl font-bold text-center mt-5">Today pick</h1>
+        <section className="text-white w-full my-20">
+            <h1 className="text-5xl font-bold text-center mt-5 ">
+                Today&apos;s pick
+            </h1>
 
-            <div className="grid place-items-center mx-auto w-[900px] h-[700px] relative ">
-                <div className="absolute top-0 left-0 grid   place-items-center justify-center bg-main-background opacity-0 hover:opacity-90 z-10 h-full w-full">
-                    <div>
-                        <Link href="/arts/1">
-                            <a>
-                                <h1 className="text-5xl font-bold">
-                                    Cutie Cat
-                                </h1>
-                            </a>
-                        </Link>
+            <div className="w-[900px] mx-auto mt-20">
+                <Link href="/arts/1">
+                    <a>
+                        <h1 className="text-5xl font-bold rays gradient_text w-max">
+                            Cutie Cat
+                        </h1>
+                    </a>
+                </Link>
 
-                        <p className="text-right my-7 text-xl">
-                            by:{' '}
-                            <Link href="/artists/1">
-                                <a className="text-yellow-300">Crazy Artist</a>
-                            </Link>
-                        </p>
-                    </div>
-                </div>
+                <p className="my-7 text-xl">
+                    by:{' '}
+                    <Link href="/artists/1">
+                        <a className="text-yellow-300">Crazy Artist</a>
+                    </Link>
+                </p>
+            </div>
+            <div className="grid place-items-center mx-auto w-[900px] h-[700px] relative mt-10">
                 <Image
-                    src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+                    src="https://images.unsplash.com/photo-1582201957424-621320ad670d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1009&q=80"
                     alt="Today pick"
                     layout="fill"
                     objectFit="contain"
                 />
             </div>
-        </div>
+        </section>
     )
 }
 
