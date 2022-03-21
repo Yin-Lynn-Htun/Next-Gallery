89764438a55c94028d5182dd9c15218cb70ad9b2
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-03-21 23:23:31 +0630

    feat: Added feature section in homepage

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import { TodayPick } from './TodayPick'
+import TodayPick from '../components/HomePage/TodayPick'
 import HeroSection from '../components/HomePage/HeroSection'
 import TrendingArtist from '../components/HomePage/TrendingArtists'
 import Wrapper from '../components/Wrapper'
-import Image from 'next/image'
-import Link from 'next/link'
+import Features from '../components/HomePage/Features'
 
 export default function Home() {
     return (
         <Wrapper>
             <HeroSection />
+            <Features />
             <TodayPick />
             <TrendingArtist />
         </Wrapper>
