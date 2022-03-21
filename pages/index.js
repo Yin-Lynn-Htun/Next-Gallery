38f2e028b6199cf220f7c36e8f0622dbf7db7ef7
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-03-22 00:42:58 +0630

    feat: Added footer in homepage

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import HeroSection from '../components/HomePage/HeroSection'
 import TrendingArtist from '../components/HomePage/TrendingArtists'
 import Wrapper from '../components/Wrapper'
 import Features from '../components/HomePage/Features'
+import Footer from '../components/Footer'
 
 export default function Home() {
     return (
@@ -11,6 +12,7 @@ export default function Home() {
             <Features />
             <TrendingArtist />
             <TodayPick />
+            <Footer />
         </Wrapper>
     )
 }
