131f189a7d4c9918d615ca13d0d905ee4592d0f7
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-03-21 23:37:09 +0630

    feat: Change whitespace in homepage's sections

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,8 @@ export default function Home() {
         <Wrapper>
             <HeroSection />
             <Features />
-            <TodayPick />
             <TrendingArtist />
+            <TodayPick />
         </Wrapper>
     )
 }
