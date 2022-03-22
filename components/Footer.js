5735709121cca4b33eee952c4a9cf335646b4943
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-03-22 14:33:06 +0630

    feat: Added link in footer

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ import Image from 'next/image'
 
 const Footer = () => {
     return (
-        <footer className="text-white my-20 w-full">
+        <footer className="text-white mt-20 my-10 w-full">
             <div className="grid grid-cols-3 ">
                 <div className="flex flex-col gap-5">
                     <div className="flex items-center ">
@@ -30,35 +30,73 @@ const Footer = () => {
                         around the world. Enjoy the art.
                     </p>
                     <div className="flex gap-5 my-3">
-                        <div className="w-10 h-10 bg-form-gray rounded-full grid place-items-center">
-                            <Image
-                                src={'/static/images/footer/FacebookLogo.svg'}
-                                width={30}
-                                height={30}
-                                alt="Logo"
-                                className="cursor-pointer"
-                            />
-                        </div>
-                        <div className="w-10 h-10 bg-form-gray rounded-full grid place-items-center">
-                            <Image
-                                src={'/static/images/footer/LinkedInLogo.svg'}
-                                width={30}
-                                height={30}
-                                alt="Logo"
-                                className="cursor-pointer"
-                            />
-                        </div>
+                        <Link href="/">
+                            <a>
+                                <div className="w-10 h-10 bg-form-gray rounded-full grid place-items-center">
+                                    <Image
+                                        src={
+                                            '/static/images/footer/FacebookLogo.svg'
+                                        }
+                                        width={30}
+                                        height={30}
+                                        alt="Logo"
+                                    />
+                                </div>
+                            </a>
+                        </Link>
+                        <Link href="/">
+                            <a>
+                                <div className="w-10 h-10 bg-form-gray rounded-full grid place-items-center">
+                                    <Image
+                                        src={
+                                            '/static/images/footer/LinkedInLogo.svg'
+                                        }
+                                        width={30}
+                                        height={30}
+                                        alt="Logo"
+                                    />
+                                </div>
+                            </a>
+                        </Link>
                     </div>
                 </div>
 
                 <div className="list-disc flex flex-col gap-2 justify-self-center text-lg ">
-                    <li>Home</li>
-                    <li>Arts</li>
-                    <li>Artists</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                    <li>Sign Up</li>
-                    <li>Login</li>
+                    <li>
+                        <Link href="/">
+                            <a> Home</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/arts">
+                            <a> Arts</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/artists">
+                            <a> Artists</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/">
+                            <a> About Us</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/">
+                            <a> Contact Us</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/signup">
+                            <a> Sign Up</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/login">
+                            <a> Login</a>
+                        </Link>
+                    </li>
                 </div>
 
                 <div className="flex flex-col gap-10">
@@ -103,7 +141,7 @@ const Footer = () => {
                 </div>
             </div>
 
-            <p className="text-center mt-14 text-lg">
+            <p className="text-center mt-10 text-lg">
                 Developed by{' '}
                 <span className="text-blue-300"> Yin Lynn Htun </span>. &copy;
                 Copyright 2022-2023, All Right Reserved
