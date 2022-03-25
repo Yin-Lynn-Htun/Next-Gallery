9bb012a9e851e5f626cf9b397ef65f63ad92e042
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-03-25 19:34:01 +0630

    feat: add compare password function

--- utils/auth.js
+++ utils/auth.js
@@ -4,3 +4,7 @@ export const hashPassword = (password) => {
     const hashedPassword = bcrypt.hashSync(password, 8)
     return hashedPassword
 }
+
+export const isPasswordValid = (password, hashedPassword) => {
+    return bcrypt.compareSync(password, hashedPassword)
+}
