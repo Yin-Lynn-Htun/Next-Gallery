491c24ad84aa45b41c0ee9924fa43f41d60dfeb0
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-03-26 23:19:16 +0630

    feat: Add data model for artist

---
+++ Models/Artist.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose')
+
+const ArtistScheme = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+        maxLength: [20, 'Username must be less than 20 characters'],
+        trim: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+        lowercase: true,
+        trim: true,
+        validate: {
+            validator: function (v) {
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v)
+            },
+            message: (props) => `${props.value} is not a valid email!`,
+        },
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minLength: [8, 'Password must be at least 8 characters'],
+        trim: true,
+    },
+    firstName: {
+        type: String,
+    },
+    lastName: {
+        type: String,
+    },
+    bio: {
+        type: String,
+    },
+    imgUrl: {
+        type: String,
+    },
+    social: {
+        website: {
+            type: String,
+        },
+        facebook: {
+            type: String,
+        },
+        instagram: {
+            type: String,
+        },
+        twitter: {
+            type: String,
+        },
+    },
+    location: {
+        type: String,
+    },
+})
+
+module.exports =
+    mongoose.models.Artist || mongoose.model('Artist', ArtistScheme)
