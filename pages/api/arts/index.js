548ca4ecb0aa8c4837fc02dfb60006f5468973a5
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-03-27 12:11:05 +0630

    feat: Create API for all arts

--- pages/api/arts/index.js
+++ pages/api/arts/index.js
@@ -2,22 +2,29 @@ import { connectToDb } from '../../../utils/db'
 import Art from '../../../Models/Art'
 
 const handler = async (req, res) => {
-    connectToDb()
-    if (req.method !== 'POST') {
-        return res.status(405).send('Method Not Allowed')
+    await connectToDb()
+    if (req.method === 'POST') {
+        try {
+            const art = await Art.create(req.body)
+
+            return res
+                .status(200)
+                .send({ ok: true, message: 'Art added', data: art })
+        } catch (err) {
+            console.log(err)
+            return res.status(400).send({ ok: false, error: err.message })
+        }
     }
 
-    try {
-        const art = await Art.create(req.body)
-        console.log(art)
-
-        return res
-            .status(200)
-            .send({ ok: true, message: 'Art added', data: art })
-    } catch (err) {
-        console.log(err)
-        return res.status(400).send({ ok: false, error: err.message })
+    if (req.method === 'GET') {
+        const arts = await Art.find()
+            .populate('artist', 'username')
+            .select('-__v')
+        // const arts = await Art.find()
+        return res.status(200).send({ ok: true, data: arts })
     }
+
+    return res.status(405).send('Method Not Allowed')
 }
 
 export default handler
