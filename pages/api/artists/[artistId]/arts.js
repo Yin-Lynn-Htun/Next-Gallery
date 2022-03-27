488ef69ff72cbd0c230cb4db4dbdf0bc204db267
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-03-27 11:57:01 +0630

    feat: Create API for aritist detail

--- pages/api/artists/[artistId]/arts.js
+++ pages/api/artists/[artistId]/arts.js
@@ -7,7 +7,7 @@ const handler = async (req, res) => {
 
     const arts = await Arts.find({ artist: artistId })
 
-    return res.status(200).send({ message: 'Create a profile', data: arts })
+    return res.status(200).send({ ok: true, data: arts })
 }
 
 export default handler
