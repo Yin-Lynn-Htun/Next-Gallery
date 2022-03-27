ff47174fa5d2abbf065c0cd56f7abc6e52f9bfa8
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-03-27 12:02:30 +0630

    feat: API for artist detail, update and delete

--- pages/api/artists/[artistId]/index.js
+++ pages/api/artists/[artistId]/index.js
@@ -1,6 +1,47 @@
+import Artist from '../../../../Models/Artist'
+
 const handler = async (req, res) => {
-    console.log(req.query)
-    return res.status(200).send({ message: 'Create a profile', data: req.body })
+    const { artistId } = req.query
+
+    if (req.method === 'GET') {
+        try {
+            const artist = await Artist.findById(artistId)
+            if (!artist) {
+                return res
+                    .status(404)
+                    .send({ ok: false, message: 'Artist not found' })
+            }
+            return res.status(200).send({ ok: true, data: artist })
+        } catch (err) {
+            return res
+                .status(404)
+                .send({ ok: false, message: 'Artist not found' })
+        }
+    }
+
+    if (req.method === 'PUT') {
+        try {
+            const artist = await Artist.findByIdAndUpdate(artistId, req.body, {
+                new: true,
+            })
+            return res.status(200).send({ ok: true, data: artist })
+        } catch (err) {
+            return res
+                .status(500)
+                .send({ ok: false, message: 'Error updating artist' })
+        }
+    }
+
+    if (req.method === 'DELETE') {
+        try {
+            const artist = await Artist.findByIdAndDelete(artistId)
+            return res.status(200).send({ ok: true, data: artist })
+        } catch (err) {
+            return res
+                .status(500)
+                .send({ ok: false, message: 'Error deleting artist' })
+        }
+    }
 }
 
 export default handler
