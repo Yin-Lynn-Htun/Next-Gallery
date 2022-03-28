be0953225f189db3e9aedd0b1f4ce978a1c281ab
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-03-29 01:52:48 +0630

    feat: Added Preview component for cropped image

--- components/ImageCrop/ImageCrop.js
+++ components/ImageCrop/ImageCrop.js
@@ -1,31 +1,82 @@
-import { useState } from 'react'
+/* eslint-disable @next/next/no-img-element */
+import { useRef, useState } from 'react'
 import ReactCrop from 'react-image-crop'
 import 'react-image-crop/dist/ReactCrop.css'
 import Image from 'next/image'
+import { useDebounceEffect } from './useDebounceEffect'
+import { canvasPreview } from './CanvasPreview'
 
 const ImageCrop = ({ imgSrc }) => {
+    const previewCanvasRef = useRef(null)
+    const [completedCrop, setCompletedCrop] = useState()
+    const imgRef = useRef(null)
+
     const [crop, setCrop] = useState({
-        unit: '%', // Can be 'px' or '%'
+        unit: 'px', // Can be 'px' or '%'
         x: 25,
         y: 25,
-        width: 50,
-        height: 50,
+        width: 100,
+        height: 100,
     })
+
+    useDebounceEffect(
+        async () => {
+            if (
+                completedCrop?.width &&
+                completedCrop?.height &&
+                imgRef.current &&
+                previewCanvasRef.current
+            ) {
+                // We use canvasPreview as it's much faster than imgPreview.
+                canvasPreview(
+                    imgRef.current,
+                    previewCanvasRef.current,
+                    completedCrop
+                )
+            }
+        },
+        100,
+        [completedCrop]
+    )
+
     return (
-        <ReactCrop
-            crop={crop}
-            onChange={(c) => setCrop(c)}
-            circularCrop={true}
-            aspect={1 / 1}
-        >
-            <Image
-                src={imgSrc}
-                alt="Profile Picture"
-                width={500}
-                height={500}
-                objectFit="cover"
-            />
-        </ReactCrop>
+        <div className="flex gap-20">
+            <div className="w-[500px] h-max">
+                <ReactCrop
+                    crop={crop}
+                    onChange={(c) => setCrop(c)}
+                    onComplete={(c) => setCompletedCrop(c)}
+                    circularCrop={true}
+                    aspect={1 / 1}
+                >
+                    <img
+                        ref={imgRef}
+                        src={imgSrc}
+                        alt="Profile Picture"
+                        width={500}
+                        height={500}
+                        style={{
+                            objectFit: 'cover',
+                            border: '1px solid white',
+                        }}
+                    />
+                </ReactCrop>
+            </div>
+            <div className="w-[250px] h-[250px]">
+                {Boolean(completedCrop) && (
+                    <canvas
+                        ref={previewCanvasRef}
+                        style={{
+                            outline: '1px solid white',
+                            borderRadius: '50%',
+                            objectFit: 'contain',
+                            width: 250,
+                            height: 250,
+                        }}
+                    />
+                )}
+            </div>
+        </div>
     )
 }
 
