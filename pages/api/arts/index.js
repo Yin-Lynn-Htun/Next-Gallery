346bd5ceac5fdc46e3101a681636f8fec5221922
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-04-12 17:09:12 +0630

    feat: Upload artist when the art is uploaded

--- pages/api/arts/index.js
+++ pages/api/arts/index.js
@@ -1,11 +1,21 @@
 import { connectToDb } from '../../../utils/db'
 import Art from '../../../Models/Art'
+import { getSession } from 'next-auth/react'
 
 const handler = async (req, res) => {
     await connectToDb()
+    const session = await getSession({ req })
+
+    // console.log(session)
+    // { user: {email: 'yinlynnhtun@gmail.com'}, userId: '623...'    }
+
     if (req.method === 'POST') {
         try {
-            const art = await Art.create(req.body)
+            const art = await Art.create({
+                ...req.body,
+                artist: session.userId,
+            })
+            console.log(art)
 
             return res
                 .status(200)
