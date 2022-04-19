da0a00a1d0adea31d0ee43c62552c1c4f77ab117
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-04-19 23:58:51 +0630

    feat: Added about me for artist profile

---
+++ components/Profile/AboutMe.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const AboutMe = () => {
+    return (
+        <div className="my-10">
+            <h1 className="font-bold text-3xl">About the artist</h1>
+            <div className="my-5 text-lg leading-relaxed">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Accusantium vero reprehenderit similique!
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quibusdam sit quae unde ratione harum exercitationem magni
+                    voluptate excepturi, amet molestias vitae autem illo sed
+                    nesciunt, sint quo mollitia corporis perspiciatis aliquid.
+                </p>
+
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Incidunt, aliquid distinctio? Explicabo itaque asperiores
+                    laboriosam cupiditate molestias quas consequatur culpa,
+                    tempore nulla, doloremque quam ducimus qui maxime inventore,
+                    labore hic voluptate eos adipisci placeat cum nihil sint
+                    veritatis rerum impedit! Quaerat ducimus blanditiis
+                    assumenda nobis. Fugiat dolorum id officia. Eius, iure omnis
+                    recusandae obcaecati cum ipsa temporibus voluptates possimus
+                    expedita!
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default AboutMe
