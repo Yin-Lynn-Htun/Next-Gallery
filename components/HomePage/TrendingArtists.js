b04c08e8628b84d4a502f8992035fc2267f95316
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-04-22 12:25:45 +0630

    feat: Use data from DB in trending Artists

--- components/HomePage/TrendingArtists.js
+++ components/HomePage/TrendingArtists.js
@@ -1,16 +1,14 @@
 import AristsItem from '../AristsItem'
-import { dummy_artists } from '../../dummy_data'
-import Link from 'next/link'
 
-const TrendingArtist = () => {
+const TrendingArtist = ({ artists }) => {
     return (
         <section className="my-20">
             <h1 className="text-white text-5xl font-bold text-center">
                 Trending Artists
             </h1>
             <div className="flex flex-wrap justify-between gap-7 mt-10">
-                {dummy_artists.map((artist) => (
-                    <AristsItem key={artist.id} {...artist} />
+                {artists.map((artist) => (
+                    <AristsItem key={artist._id} {...artist} />
                 ))}
             </div>
         </section>
--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,28 @@ import TrendingArtist from '../components/HomePage/TrendingArtists'
 import Wrapper from '../components/Wrapper'
 import Features from '../components/HomePage/Features'
 import Footer from '../components/Footer'
+import Artist from '../Models/Artist'
+import { connectToDb } from '../utils/db'
 
-export default function Home() {
+export default function Home({ artists }) {
     return (
         <Wrapper>
             <HeroSection />
             <Features />
-            <TrendingArtist />
+            <TrendingArtist artists={artists} />
             <TodayPick />
             <Footer />
         </Wrapper>
     )
+}
+
+export async function getStaticProps(context) {
+    await connectToDb()
+    const data = await Artist.find({}).sort({ watch: -1 }).limit(5)
+    const artists = JSON.parse(JSON.stringify(data))
+    return {
+        props: {
+            artists,
+        }, // will be passed to the page component as props
+    }
 }
