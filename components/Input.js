7fbc6af78dbab88c780f666242607ed78c1fba0a
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-04-24 00:31:43 +0630

    fix: Fix bug in sign up and login form

--- components/Input.js
+++ components/Input.js
@@ -10,13 +10,13 @@ const Input = ({ label, id, errorMessage, touched, ...props }) => {
                 {...props}
                 className="rounded-lg my-1 w-full px-5 py-2 text-black outline-none text-base"
             />
-            <small
-                className={`text-red-400 text-base ${
-                    touched && errorMessage ? 'opacity-1' : 'opacity-0'
-                } `}
-            >
-                {errorMessage ? errorMessage : 'no error'}
-            </small>
+            {touched && errorMessage ? (
+                <small className={`text-red-400 text-base `}>
+                    {errorMessage}
+                </small>
+            ) : (
+                <small className="text-base opacity-0">No error</small>
+            )}
         </div>
     )
 }
