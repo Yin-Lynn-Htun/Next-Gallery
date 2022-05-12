8ba88c566770ab13ec6e2d5a56ff18278838e9da
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-05-12 16:52:41 +0630

    feat: Change styling of artist item

--- components/AristsItem.js
+++ components/AristsItem.js
@@ -23,7 +23,7 @@ const AristsItem = ({ _id, username, imgUrl, popularity, arts }) => {
     return (
         <Link href={`/artists/${_id}`}>
             <a className="flex">
-                <div className="p-10 rounded-xl gap-5 border-blue-400/0 border-2 my-10 items-center flex flex-col text-white cursor-pointer w-[300px] justify-center bg-text-darker ">
+                <div className="p-10 rounded-xl gap-3 border-blue-400/0 border-2 my-10 items-center flex flex-col text-white cursor-pointer w-[250px] justify-center bg-transparent hover:bg-text-darker ">
                     <div className="row-span-2 w-[150px] h-[150px] justify-self-center self-center relative outline-text-pink outline-2 outline-none rounded-full">
                         <Image
                             src={imgUrl || '/static/images/defaultUser.svg'}
@@ -33,7 +33,9 @@ const AristsItem = ({ _id, username, imgUrl, popularity, arts }) => {
                             objectFit="cover"
                         />
                     </div>
-                    <h1 className="text-2xl font-bold w-max ">{username}</h1>
+                    <h1 className="text-2xl mt-5 font-bold w-max ">
+                        {username}
+                    </h1>
                     <h1 className="text-xl font-bold w-max text-green-200">
                         {artCount ? artCount : '0'} Artworks
                     </h1>
@@ -41,20 +43,6 @@ const AristsItem = ({ _id, username, imgUrl, popularity, arts }) => {
                         <GiSelfLove className="text-red-500 w-10 h-10 inline mr-5" />
                         <span className="text-white">{popularity || 12}</span>
                     </div>
-                    {/* <h1 className="text-lg font-bold text-center w-full text-yellow-200 overflow-hidden whitespace-nowrap text-ellipsis ">
-                        {location}
-                    </h1> */}
-
-                    {/* <div className="flex justify-between my-5 w-full">
-                        <div>
-                            <GiSelfLove className="text-red-500 w-10 h-10 inline mr-5" />
-                            <span className="text-white">{loveCount}</span>
-                        </div>
-                        <div>
-                            <AiFillEye className="text-blue-500 w-10 h-10 inline mr-5" />
-                            <span className="text-white">{viewCount}</span>
-                        </div>
-                    </div> */}
                 </div>
             </a>
         </Link>
