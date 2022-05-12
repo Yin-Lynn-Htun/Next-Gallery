bb863e7ead75a2c80de1f39a6a56326e1968629d
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-05-12 17:05:06 +0630

    feat: Change today pick links

--- components/HomePage/TodayPick.js
+++ components/HomePage/TodayPick.js
@@ -12,26 +12,30 @@ const TodayPick = () => {
             <div className="grid place-items-center">
                 <div className="mx-auto mt-5 lg:mt-20 flex flex-col lg:flex-row items-baseline">
                     <h1 className="text-3xl lg:text-5xl font-bold rays gradient_text w-max">
-                        <Link href="/arts/1">
-                            <a> Cutie Cat </a>
+                        <Link href="/arts/627ce128eae3c7f360b97b69">
+                            <a>Bitcoin !!! </a>
                         </Link>
                     </h1>
 
                     <p className="text-xl ml-2 lg:ml-4">
                         by:{' '}
-                        <Link href="/artists/1">
-                            <a className="text-yellow-300">Crazy Artist</a>
+                        <Link href="/artists/6278c9af783660a992d1029e">
+                            <a className="text-yellow-300">mary</a>
                         </Link>
                     </p>
                 </div>
             </div>
             <div className="grid place-items-center mx-auto w-11/12 h-96 lg:w-[900px] lg:h-[700px] relative mt-10">
-                <Image
-                    src="https://images.unsplash.com/photo-1582201957424-621320ad670d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1009&q=80"
-                    alt="Today pick"
-                    layout="fill"
-                    objectFit="cover"
-                />
+                <Link href="/arts/627ce128eae3c7f360b97b69">
+                    <a>
+                        <Image
+                            src="https://images.unsplash.com/photo-1609554496796-c345a5335ceb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                            alt="Today pick"
+                            layout="fill"
+                            objectFit="cover"
+                        />
+                    </a>
+                </Link>
             </div>
         </section>
     )
